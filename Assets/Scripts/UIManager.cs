0fa9f192a8f744543df2ee44297baf33090f4c02
FloreauLuca <lucafloreau@akeonet.com>
2024-04-07 00:03:10 +0200

    Finish integrate map2

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,10 +8,12 @@ public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private InteractionPanel interactionPanel = null;
     [SerializeField] private TextMeshProUGUI _textMeshPro = null;
+    [SerializeField] private GameObject _startMenu = null;
 
     private void Start()
     {
         HideInteractionPanel();
+        _startMenu.SetActive(true);
     }
 
     public void ShowInteraction(Monkenemy monkenemy, InteractionParameters interactionParameters)
